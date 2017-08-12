0d5bd592c5be528e164aecd3e80b2afe40befb17
RISCassembler <coffee5.56natosystemcall@gmail.com>
2017-08-12 18:59:56 +0900

    make it display game's information.

--- CapsLockDataRegister/src/capslockdataregister/MainFormController.java
+++ CapsLockDataRegister/src/capslockdataregister/MainFormController.java
@@ -5,10 +5,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -20,12 +26,20 @@ import org.json.JSONObject;
  */
 public class MainFormController implements Initializable {
 
-    List<GameCertification> certifications = new ArrayList();
+  
+    @FXML TableView<GameRecord> GameInfoView;
+    @FXML TableColumn<GameRecord, String> UUIDCol;
+    @FXML TableColumn<GameRecord, String> NameCol;
+    @FXML TableColumn<GameRecord, String> ExecutableCol;
+    @FXML TableColumn<GameRecord, String> VersionCol;
+    @FXML TableColumn<GameRecord, String> ImageCol;
+    @FXML TableColumn<GameRecord, String> MovieCol;
+    
+    ObservableList<GameRecord> DisplayCollection = FXCollections.observableArrayList();
     
     @Override
     public void initialize(URL url, ResourceBundle rb){
         if(!LoadJSONDatabase())System.err.println("failed");
-        certifications.forEach(e -> e.dump());
     }    
     
     private boolean LoadJSONDatabase(){
@@ -49,9 +63,49 @@ public class MainFormController implements Initializable {
         
         System.out.println(jsonString);
         
-        JSONArray JSONDB = new JSONArray(jsonString);
-        JSONDB.forEach(record -> certifications.add(new GameCertification((JSONObject)record)));
+        UUIDCol.setCellValueFactory(new PropertyValueFactory<>("uuid"));
+        NameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        ExecutableCol.setCellValueFactory(new PropertyValueFactory<>("executable"));
+        VersionCol.setCellValueFactory(new PropertyValueFactory<>("version"));
+        ImageCol.setCellValueFactory(new PropertyValueFactory<>("image"));
+        MovieCol.setCellValueFactory(new PropertyValueFactory<>("movie"));
+
+        new JSONArray(jsonString).forEach(record -> DisplayCollection.add(new GameRecord((JSONObject)record)));
+        
+        GameInfoView.setItems(DisplayCollection);
+        
+        System.err.println(DisplayCollection.get(0).uuid);
+        System.err.println(DisplayCollection.get(0).name);
+        System.err.println(DisplayCollection.get(0).executable);
+        System.err.println(DisplayCollection.get(0).version);
+        System.err.println(DisplayCollection.get(0).image);
+        System.err.println(DisplayCollection.get(0).movie);
         
         return true;
+    }
+    
+    public static class GameRecord{
+        private final SimpleStringProperty uuid;
+        private final SimpleStringProperty name;
+        private final SimpleStringProperty executable;
+        private final SimpleStringProperty version;
+        private final SimpleStringProperty image;
+        private final SimpleStringProperty movie;
+        
+        private GameRecord(JSONObject record){
+            uuid = new SimpleStringProperty(record.getString("UUID"));
+            name = new SimpleStringProperty(record.getString("name"));
+            executable = new SimpleStringProperty(record.getString("executable"));
+            version = new SimpleStringProperty(record.getString("version"));
+            image = new SimpleStringProperty(record.getJSONArray("image").join(","));
+            movie = new SimpleStringProperty(record.getJSONArray("movie").join(","));
+        }
+        
+        public StringProperty uuidProperty(){return uuid;}
+        public StringProperty nameProperty(){return name;}
+        public StringProperty executableProperty(){return executable;}
+        public StringProperty versionProperty(){return version;}
+        public StringProperty imageProperty(){return image;}
+        public StringProperty movieProperty(){return movie;}
     }
 }
