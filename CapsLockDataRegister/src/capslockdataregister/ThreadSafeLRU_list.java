59d50f6a7afb16ef860f4ad439239179805db0ae
RISCassembler <coffee5.56natosystemcall@gmail.com>
2017-10-20 08:19:04 +0900

    Implement LRU list.But this class is still thread-unsafe.

--- CapsLockDataRegister/src/capslockdataregister/ThreadSafeLRU_list.java
+++ CapsLockDataRegister/src/capslockdataregister/ThreadSafeLRU_list.java
@@ -1,8 +1,20 @@
 package capslockdataregister;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
- *
+ * Least Recently Used list
  */
-public class ThreadSafeLRU_list {
+class ThreadSafeLRU_list<K, V> extends LinkedHashMap<K, V>{
+    private static final int MAX_SIZE = 10;
+
+    ThreadSafeLRU_list() {
+        super(MAX_SIZE, 0.75F, true);
+    }
     
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > MAX_SIZE;
+    }
 }
