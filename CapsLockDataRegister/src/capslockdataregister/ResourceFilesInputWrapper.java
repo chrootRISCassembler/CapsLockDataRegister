f844ebdc29ca51c2a1307f3ea9fb27571fd155d1
RISCassembler <coffee5.56natosystemcall@gmail.com>
2017-10-20 19:18:22 +0900

    Implemented ResourceFilesInputWrapper class.

--- CapsLockDataRegister/src/capslockdataregister/ResourceFilesInputWrapper.java
+++ CapsLockDataRegister/src/capslockdataregister/ResourceFilesInputWrapper.java
@@ -1,6 +1,7 @@
 package capslockdataregister;
 
 import java.util.UUID;
+import java.util.function.Supplier;
 
 /**
  *
@@ -10,4 +11,12 @@ enum ResourceFilesInputWrapper {
     
     private final ThreadSafeLRU_list<UUID, LauncherResourceFilesValidator> LRUlist = new ThreadSafeLRU_list<>();
     
+    LauncherResourceFilesValidator add(UUID uuid, Supplier<LauncherResourceFilesValidator> CreatorLambda){
+        LauncherResourceFilesValidator retValidator = LRUlist.get(uuid);
+        if(retValidator != null)return retValidator;
+        
+        retValidator = CreatorLambda.get();
+        LRUlist.put(uuid, retValidator);
+        return retValidator;
+    }
 }
