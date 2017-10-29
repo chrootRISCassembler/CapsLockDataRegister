3e05d2dbb67cec480a3fa254082162b1d63366f4
RISCassembler <coffee5.56natosystemcall@gmail.com>
2017-10-30 01:27:34 +0900

    Add two function areValidImages() and parseFiles() to Validator class. But areValidImages() doesn't use cache yet.

--- CapsLockDataRegister/src/capslockdataregister/LauncherResourceFilesValidator.java
+++ CapsLockDataRegister/src/capslockdataregister/LauncherResourceFilesValidator.java
@@ -5,6 +5,7 @@ import java.nio.file.ClosedWatchServiceException;
 import java.nio.file.FileSystems;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
@@ -14,6 +15,8 @@ import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -124,5 +127,81 @@ class LauncherResourceFilesValidator extends Thread{
         }catch(NullPointerException | IllegalArgumentException e){
         }
         return false;
+    }
+    
+    /**
+     * Verify images.
+     * <p>Each path must be quoted by ".</p>
+     * <p>Ex) "aaa", "bbb"</p>
+     * @param Images Array of path.
+     * @return Either all images are valid.
+     */
+    static final boolean areValidImages(String Images){
+        if(Images.isEmpty())return false;
+        try{
+            for(final Path path : parseFiles(Images)){
+                if(!Files.isRegularFile(path))return false;
+                new Image(path.toUri().toString());
+            }
+        }catch(NullPointerException | IllegalArgumentException ex){
+            return false;
+        }
+        return true;
+    }
+    
+    /**
+     * Parse String of path array.
+     * <p>Each path must be quoted by ".</p>
+     * <p>Ex) "aaa", "bbb"</p>
+     * @param source Array of path.
+     * @return Set of parsed file's Path.
+     * <h2>This function is incomplete.Someone fix it!</h2>
+     */
+    static final Set<Path> parseFiles(String source) throws InvalidPathException{
+        boolean InDoubleQuotation = false;
+        boolean IsEscaped = false;
+        boolean IsWatingTokenFirst = false;
+        int TokenFirst = 0;
+        
+        final Set<Path> PathSet = new HashSet<>();
+        
+        for(int i = 0;i != source.length();++i){
+            final char ch = source.charAt(i);
+            switch(ch){
+                case '\"':
+                    if(IsEscaped){
+                        IsEscaped = false;
+                        continue;
+                    }
+                    
+                    if(InDoubleQuotation){
+                        PathSet.add(Paths.get(source.substring(TokenFirst, i)));
+                        InDoubleQuotation = false;
+                    }else{
+                        InDoubleQuotation = true;
+                        IsWatingTokenFirst = true;
+                    }
+                break;
+                case ',':
+                    if(InDoubleQuotation)continue;
+                    
+                break;
+                case ' ':
+                    
+                break;
+                default:
+                    if(IsWatingTokenFirst){
+                        TokenFirst = i;
+                        IsWatingTokenFirst = false;
+                    }
+                    
+                    if(IsEscaped){
+                        IsEscaped = false;
+                        continue;
+                    }
+                    break;
+            }
+        }
+        return PathSet;
     }
 }
