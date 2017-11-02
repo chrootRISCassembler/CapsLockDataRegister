597b0cd445de8af04439725306073d051d66d456
RISCassembler <coffee5.56natosystemcall@gmail.com>
2017-11-03 00:05:10 +0900

    Add toPermString() to Command class. Its' the surjective function form '0'-'7' to "XYZ". ex) '7' -> "rwx"

--- CapsLockDataRegister/src/capslockdataregister/Command.java
+++ CapsLockDataRegister/src/capslockdataregister/Command.java
@@ -13,4 +13,32 @@ final class Command {
     static void dump(final String[] ParsedStringArray, MainFormController controller){
         controller.DisplayCollection.forEach(ele -> System.err.println(ele.geJSON()));
     }
+
+    /**
+     * 0-7の文字に対して"rwx"のようなパーミッションを表す三文字を返す.
+     * <p>PosixFilePermissions.fromString()に渡す引数を構築するときに使う</p>
+     * <p>0から7以外の文字を渡すと"---"を返す</p>
+     * @param num 0から7の文字.本物のchmodに渡す700のような引数の一文字分.
+     * @return "rwx"とか"r--"のような三文字.
+     */
+    static final String toPermString(char num){
+        switch(num){
+            case '1':
+                return "--x";
+            case '2':
+                return "-w-";
+            case '3':
+                return "-wx";
+            case '4':
+                return "r--";
+            case '5':
+                return "r-x";
+            case '6':
+                return "rw-";
+            case '7':
+                return "rwx";
+            default:
+                return "---";
+        }
+    }
 }
