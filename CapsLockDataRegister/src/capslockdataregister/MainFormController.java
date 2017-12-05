b1bea63ac658cf06457262080db24ba86a656315
RISCassembler <coffee5.56natosystemcall@gmail.com>
2017-12-06 00:56:09 +0900

    Add GPL header.

--- CapsLockDataRegister/src/capslockdataregister/MainFormController.java
+++ CapsLockDataRegister/src/capslockdataregister/MainFormController.java
@@ -1,7 +1,24 @@
+/*  
+    This file is part of CapsLockDataRegister. CapsLockDataRegister is a JSON generator for CapsLock.
+    Copyright (C) 2017 RISCassembler
+
+    CapsLockDataRegister is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package capslockdataregister;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
